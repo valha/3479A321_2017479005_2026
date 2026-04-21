faf5e6632cb182195f5d9acf40eff713e86fcec7
AlanC <aa.chovan@gmail.com>
2026-04-21 13:40:50 -0400

    desktop push
    push de 29 archivos que necesitaba el primer laboratorio

---
+++ Lab 1/lab_1_test/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Alan\\Desktop\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Alan\\Documents\\GitHub\\3479A321_2017479005\\Lab 1\\lab_1_test" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Alan\\Desktop\\flutter"
+  "PROJECT_DIR=C:\\Users\\Alan\\Documents\\GitHub\\3479A321_2017479005\\Lab 1\\lab_1_test"
+  "FLUTTER_ROOT=C:\\Users\\Alan\\Desktop\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Alan\\Documents\\GitHub\\3479A321_2017479005\\Lab 1\\lab_1_test\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Alan\\Documents\\GitHub\\3479A321_2017479005\\Lab 1\\lab_1_test"
+  "FLUTTER_TARGET=C:\\Users\\Alan\\Documents\\GitHub\\3479A321_2017479005\\Lab 1\\lab_1_test\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049Y2MwNzM0YWM3MQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTlhYTU4NGZkZg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS41"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Alan\\Documents\\GitHub\\3479A321_2017479005\\Lab 1\\lab_1_test\\.dart_tool\\package_config.json"
+)
