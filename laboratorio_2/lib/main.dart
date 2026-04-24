84c0132883458f34437331d70eb6eebe22d5b048
AlanC <aa.chovan@gmail.com>
2026-04-23 22:11:40 -0400

    creacion del tablero, no se ve al ser launcheado, espero viernes para hacer las consultas del codigo

--- laboratorio_2/lib/main.dart
+++ laboratorio_2/lib/main.dart
@@ -6,13 +6,81 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Buscaminas AC',
+      title: 'Minesweeper ACE',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: const Text('Buscaminas AC'),
+      home: const Text(
+        'Hello, Minesweeper ACE!',
+      ), // Apuntamos a nuestra nueva pantalla
+    );
+  }
+}
+
+class MinesweeperScreen extends StatelessWidget {
+  const MinesweeperScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buscaminas')),
+      body: SafeArea(
+        // Protege la UI de los bordes del dispositivo
+        child: Column(
+          // Apila el marcador arriba y el tablero abajo
+          children: [
+            // Área de Status
+            Container(
+              height: 60,
+              color: Colors.grey[300],
+              child: const Center(
+                child: Text(
+                  'STATUS: 349 segundos | Minas: 10 | Cuadros: 56',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+            ),
+            const Divider(height: 1),
+            // Área de Juego
+            Expanded(
+              // Expande el tablero para llenar la pantalla
+              child: Text(
+                'Tablero de Juego',
+                style: TextStyle(fontSize: 24, color: Colors.grey),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _gameBoard() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AspectRatio(
+          aspectRatio: 1.0, // Cuadrado perfecto
+          child: GridView.builder(
+            physics: const NeverScrollableScrollPhysics(), // Bloquea el scroll
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8, // 8 columnas
+              crossAxisSpacing: 2.0,
+              mainAxisSpacing: 2.0,
+            ),
+            itemCount: 64, // 8x8 = 64 celdas
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  border: Border.all(color: Colors.grey[600]!, width: 1.5),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
